485160179161d7bd9fc54df2fd008a572341974c
mrsayed2022 <am6776756@gmail.com>
2024-01-02 10:29:49 +0200

    first commit

--- src/app/compontes/header/page.tsx
+++ src/app/compontes/header/page.tsx
@@ -12,15 +12,15 @@ export default function Header() {
 <div   className="toggles ">
 <input type="checkbox" id="checkbox"/>
 <label onClick={()=>settoggle(!toggle)}   htmlFor="checkbox" className="toggle">
-  <div style={{background:toggle&&"white"}} className="bar bar--top"></div>
-  <div style={{background:toggle&&"white"}} className="bar bar--middle"></div>
-  <div style={{background:toggle&&"white"}} className="bar bar--bottom"></div>
+  <div style={{background:toggle?"white":undefined}} className="bar bar--top"></div>
+  <div style={{background:toggle?"white":undefined}} className="bar bar--middle"></div>
+  <div style={{background:toggle?"white":undefined}} className="bar bar--bottom"></div>
 </label>
 
 </div>
     <div className="box font-black nav">
         <nav>
-            <ul style={{height:toggle&&"40%" }} className='flex '>
+            <ul style={{height:toggle?"40%":undefined }} className='flex '>
                 
                 <li className='mr-2 font-black  sm:text-lg md:text-lg sm:mr-6 hover:text-[#52057b]  '><Link href="/">الرئيسية</Link> </li>                
                 <li className='mr-2 font-black  sm:text-lg md:text-lg sm:mr-6 hover:bg-[white] hover:text-black hover:rounded '><Link href="/compontes/book">المقررات الدراسية</Link></li>
